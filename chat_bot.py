be65d9e934d9f881b82e1e98aab281fd62dab2a6
JOAO VICTOR GARCIA <1091392223009@fatec-srv.local>
2024-06-26 21:34:23 -0300

    1

--- chat_bot.py
+++ chat_bot.py
@@ -1,7 +1,5 @@
 import sys
 import Levenshtein
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 def carregar_perguntas(arquivo):
     perguntas_respostas = {}
@@ -14,36 +12,22 @@ def carregar_perguntas(arquivo):
 def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
     menor_distancia = float("inf")
     melhor_resposta = ""
-
-    perguntas = list(perguntas_respostas.keys())
-    respostas = list(perguntas_respostas.values())
-
-    # Calcular similaridade de Levenshtein
     for p, r in perguntas_respostas.items():
         distancia = Levenshtein.distance(pergunta, p)
         if distancia < menor_distancia:
             menor_distancia = distancia
             melhor_resposta = r
-
     if menor_distancia <= limiar_distancia:
         return melhor_resposta
-
-    # Se não encontrar com Levenshtein, usar Cosine Similarity
-    vectorizer = TfidfVectorizer().fit_transform(perguntas)
-    vectors = vectorizer.transform([pergunta])
-    cosine_similarities = cosine_similarity(vectors, vectorizer).flatten()
-    highest_similarity_index = cosine_similarities.argmax()
-    if cosine_similarities[highest_similarity_index] > 0.2:  # Ajuste este valor conforme necessário
-        return respostas[highest_similarity_index]
-
-    return "Pergunta não encontrada."
+    else:
+        return "Pergunta não encontrada."
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Uso: python chat_bot.py <pergunta>")
         sys.exit(1)
 
-    pergunta = sys.argv[1].lower()
+    pergunta = sys.argv[1]
     perguntas_respostas = carregar_perguntas("perguntas.txt")
     limiar_distancia = 10
 
